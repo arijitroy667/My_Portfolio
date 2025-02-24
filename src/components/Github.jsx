77149ea50732021d62f6b9a05f1d3ccde60c9760
arijitroy667 <arijitroy0445@gmail.com>
2025-02-24 11:07:11 +0530

    Update Github.jsx

--- src/components/Github.jsx
+++ src/components/Github.jsx
@@ -10,10 +10,21 @@ function Github() {
      fetch('https://api.github.com/users/arijitroy667')
      .then(response=>response.json())
      .then(data=> {
-      console.log(data);
       setData(data)
     })
   },[])
+
+  const [repo,setRepo] = useState([])
+  useEffect(()=>{
+     fetch('https://api.github.com/users/arijitroy667/repos')
+     .then(response=>response.json())
+     .then(repo=> {
+      setRepo(repo)
+    })
+  },[])
+
+   console.log(repo[1]);
+  
 
   return (
     <div className='bg-gray-950 min-h-screen text-white'>
@@ -36,17 +47,40 @@ function Github() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            {data.repos_url}
+            {repo[1].name}
           </motion.h2>
-          <p className="text-gray-400">📅 Date: 24th Jan - 26th Jan</p>
-          <p className="text-gray-400">🏆 Type: Hackathon</p>
+          <p className="text-gray-400"><Link to='https://github.com/arijitroy667/My_Portfolio'>Click to visit</Link></p>
+          <p className="text-gray-400">🏆 Type: Web2.0</p>
           <ul className="mt-3 list-disc list-inside text-gray-300">
-            <li>🔥 Built a "Decentralized Chess Game" with 4 other members</li>
-            <li>🌐 Chain: Aptos</li>
-            <li>💻 Tech stack: Move, TypeScript, Node.js, React.js</li>
+            <li>🔥 {repo[1].description}</li>
+            <li>🌐 Commits: 10+</li>
+            <li>💻 Tech stack: {repo[1].language}</li>
           </ul>
         </motion.div>
-      </div>
+        </div>
+        <div className="flex justify-center">
+        <motion.div 
+          className="w-full max-w-lg bg-gray-900 text-white shadow-lg rounded-2xl overflow-hidden p-6 border border-teal-500"
+          whileHover={{ scale: 1.05, boxShadow: "0px 0px 15px limegreen" }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <motion.h2 
+            className="text-2xl font-semibold text-teal-400"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            {repo[0].name}
+          </motion.h2>
+          <p className="text-gray-400"><Link to='https://github.com/arijitroy667/my_First_Smart_Contract'>Click to visit</Link></p>
+          <p className="text-gray-400">🏆 Type: Web3.0</p>
+          <ul className="mt-3 list-disc list-inside text-gray-300">
+            <li>🔥 {repo[0].description}</li>
+            <li>🌐 Chain: Ethereum Sepolia</li>
+            <li>💻 Tech stack: {repo[0].language}</li>
+          </ul>
+        </motion.div>
+        </div>
       </div>
     </div>
   )
