98a67463589b10e07237145faed8dbac6b37842c
arijitroy667 <arijitroy0445@gmail.com>
2025-03-01 13:53:16 +0530

    Update Journey.jsx

--- src/components/Journey.jsx
+++ src/components/Journey.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Journey() {
   return (
-    <div>
-      
+    <div className='flex justify-center items-center h-screen bg-gray-950 text-white'>
+      <div>Currently under progress</div>
     </div>
   )
 }
